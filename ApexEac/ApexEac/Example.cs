c6b4d146870f8e268d35fe4a09e8cb0529b8a6ad
xuan525 <shanboxuan@me.com>
2019-03-08 19:11:37 +0000

    Initial commit

---
+++ ApexEac/ApexEac/Example.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ApexEac
+{
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ApexEac.Submit("playerId", "firstName", "lastName", "email@email.com", "message");
+                Console.WriteLine("Report Sent");
+            }
+            catch (ApexEac.GetVerificationException)
+            {
+                Console.WriteLine("Failed to get a token");
+            }
+            catch (ApexEac.InvalidParameterException ex)
+            {
+                Console.WriteLine("Parameter error: ");
+                Console.WriteLine(ex.Message);
+            }
+            Console.Write("Press any key to continue...");
+            Console.Read();
+        }
+    }
+}
